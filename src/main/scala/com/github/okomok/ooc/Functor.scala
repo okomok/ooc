cccd382159e359217d0fb3830c0270a163630885
unknown <okomok@gmail.com>
2012-04-06 16:22:03 +0900

    hom-functors

--- src/main/scala/com/github/okomok/ooc/Functor.scala
+++ src/main/scala/com/github/okomok/ooc/Functor.scala
@@ -32,4 +32,51 @@ object Functor {
         override def at(a: Object): Object = G.at(F.at(a))
         override def apply(f: Morphism): Morphism = G(F(f))
     }
+
+    def diagonal(C: Category): Functor = new Functor {
+        override def dom: Category = C
+        override val cod: Category = _Product_(C, C)
+        override def at(a: Object): Object = (a, a)
+        override def apply(f: Morphism): Morphism = (f, f)
+    }
+
+    def hom(C: Category, a: Object): Functor = new Functor {
+        override def dom: Category = C
+        override def cod: Category = _Set_
+        override def at(b: Object): Object = C(a, b)
+        override def apply(g: Morphism): Morphism = new Function {
+            override val dom: Set = C(a, C.dom(g))
+            override val cod: Set = C(a, C.cod(g))
+            override def apply(g: Morphism): Morphism = Function((f: Morphism) => _Set_.compose(g, f))
+        }
+    }
+
+    def chom(C: Category, b: Object): Functor = new Functor {
+        override def dom: Category = C.op
+        override def cod: Category = _Set_
+        override def at(a: Object): Object = C(a, b)
+        override def apply(f: Morphism): Morphism = new Function {
+            override val dom: Set = C(C.cod(f), b)
+            override val cod: Set = C(C.dom(f), b)
+            override def apply(f: Morphism): Morphism = Function((g: Morphism) => _Set_.compose(g, f))
+        }
+    }
+
+    def bihom(C: Category): Functor = new Functor {
+        override def dom: Category = _Product_(C.op, C)
+        override def cod: Category = _Set_
+        override def at(a_b: Object): Object = a_b match {
+            case (a, b) => C(a, b)
+        }
+        override def apply(f: Morphism): Morphism = new Function {
+            override val dom: Set = C(C.cod(f), C.dom(f))
+            override val cod: Set = C(C.dom(f), C.cod(f))
+            override def apply(h_g: Morphism): Morphism = h_g match {
+                case (h, g) => Function { (f: Morphism) =>
+                    import _Set_.o
+                    g o f o h
+                }
+            }
+        }
+    }
 }
